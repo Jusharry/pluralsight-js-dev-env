4fe94825d8f28e4fb2b1c6bb6aa0ea98a5901fc9
Harry <hfras@me.com>
2023-01-31 09:56:48 -0500

    more changes

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,10 +8,13 @@ export default {
     path: path.resolve(__dirname, "src"), //no files actually created in dev mode.
     publicPath: "/",                      //Files are served from this directory from memory
     filename: "bundle.js",
+    //hashFunction:"xxhash64" // doesnt work
+    //hashFunction:"sha512"  //doesnt work
   },
+  /*
   experiments:{
-    futureDefaults: true
-  },
+    futureDefaults: true  // not working currently
+  },*/
   plugins: [],
   module: {
     rules: [
